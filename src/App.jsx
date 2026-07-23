ed82d3c0500dd5e89b658e01f44e1a615d117164
Arpita rao <raoarpita316@gmail.com>
2026-07-23 20:01:21 +0530

    Add Balaji Burgers orange branding

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-orange-500">
+        Balaji Burgers 🍔
+      </h1>
+    </div>
+  );
+}
+
+export default App;
